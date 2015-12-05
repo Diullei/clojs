aec22a7c1a3f64ffb08a2e8a37c6779cdb75125b
Diullei <diullei@gmail.com>
2015-12-04 22:37:01 -0300

    ...

---
+++ bin/clojs.js
@@ -0,0 +1,26 @@
+// NOTE! temporary code!
+
+var closer = require('../lib/src/index');
+var escodegen = require('escodegen');
+
+var fs = require('fs');
+var path = require('path');
+
+var file = process.argv[2];
+
+var code = fs.readFileSync(file).toString();
+
+var ast = closer.parse(code, { 
+	coreIdentifier: 'core',
+	assertionsIdentifier: '___$assert' 
+});
+
+var compiled = 'var ___$closer = require("closer");\nvar core = ___$closer.core;\nvar ___$assert = ___$closer.assertions;\n\n' + escodegen.generate(ast);
+
+console.log(compiled);
+
+fs.writeFile(path.dirname(file) + '/' + path.basename(file, '.cljs') + '.js', compiled, function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+});
