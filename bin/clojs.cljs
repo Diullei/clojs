549fddffad87e8788479d40a300b9a6dc6ead0bf
Diullei <diullei@gmail.com>
2015-12-09 11:56:32 -0300

    wip

--- bin/clojs.cljs
+++ bin/clojs.cljs
@@ -1,4 +1,7 @@
-;; NOTE! temporary code!
+(def fs (require "fs"))
+(def path (require "path"))
+(def util (require "util"))
+
 (def closer (require "../lib/src/index"))
 (def escodegen (require "escodegen"))
 (def optionator (require "optionator"))
@@ -6,46 +9,63 @@
 (def core (.core closer))
 (def closerAssertions (.assertions closer))
 
-(def fs (require "fs"))
-(def path (require "path"))
+(def opt (optionator (clj->js {:prepend "Usage: cljsc [option]... [file]...\n\nUse \'cljsc\' with no options to start REPL."
+                               :append "Version {{version}}\n<http://clojs.org/>"
+                               :helpStyle {:maxPadFactor 1.9}
+                               :positionalAnywhere false
+                               :options [{:option "version"
+                                          :alias "v"
+                                          :type "Boolean"
+                                          :description "display version"}
+                                         {:option "help"
+                                          :alias "h"
+                                          :type "Boolean"
+                                          :description "display this help message"}
+                                         {:option "compile"
+                                          :alias "c"
+                                          :type "Boolean"
+                                          :description "compile to JavaScript and save as .js files"}
+                                         {:option "eval"
+                                          :alias "e"
+                                          :type "code::String"
+                                          :description "pass as string from the command line as input"}]})))
+
+(defn die!
+  [message]
+  (.error console message)
+  (.exit process 1))
+
+(defn compile-script-from-str
+	[code]
+	(do
+		(def ast
+		  (.parse closer code (clj->js {:coreIdentifier "core"})))
+		(.generate escodegen ast)))
+
+(defn compile-script
+	[file]
+	(do
+		(def code (.toString (.readFileSync fs file)))
+		(def compiled (compile-script-from-str code))
+		(.writeFile fs (apply str (.dirname path file) "/" (.basename path file ".cljs") ".js") compiled
+		            (fn [err]
+		              (if (not (nil? err))
+		                (.log console err))))))
 
 (def proc (js->clj (.argv process)))
 
-;(def opt (optionator (clj->js {:prepend "Usage: cljsc [option]... [file]...\n\nUse \'cljsc\' with no options to start REPL."
-;  										:append "Version {{version}}\n<http://clojs.org/>"
-;											:helpStyle {:maxPadFactor 1.9}
-;											:positionalAnywhere false
-;											:options [{:option "version"
-;											      		 :alias "v"
-;      										 			 :type "Boolean"
-;      										 			 :description "display version"}]})))
-;
-;(defn die [message]
-; (.error console message)
-; (.exit process 1))
-
-;(try
-;	(def parseOptions (.parse opt))
-;  (catch e
-;    (die e)))
-
-;(def generateHelp (.generateHelp opt))
-
-;; (def o (parseOptions (.argv process)))
-
-;(println (generateHelp (clj->js {:interpolate {:version "0.0.0"}})))
-
-(def file (nth proc 2))
-
-(def code (.toString (.readFileSync fs file)))
-
-(def
-  ast
-  (.parse closer code (clj->js {:coreIdentifier "core"})))
-
-(def compiled (.generate escodegen ast))
-
-(.writeFile fs (apply str (.dirname path file) "/" (.basename path file ".cljs") ".js") compiled
-            (fn [err]
-              (if (not (nil? err))
-                (.log console err))))
+(if (= (count proc) 2)
+	(def closer (require "../lib/src/start-repl"))
+	(try
+	  (do
+			(def command-options ((.parse opt) (.argv process)))
+			(if (.compile command-options)
+				(compile-script (first (._ command-options)))
+				(if (.help command-options)
+					(println ((.generateHelp opt) (clj->js {:interpolate {:version "0.0.0"}})))
+					(if (.version command-options)
+						(println "version 0.0.0")
+						(if (.eval command-options)
+							(compile-script-from-str (._ command-options)))))))
+	  (catch e
+	         (die! (.message e)))))
--- bin/clojs.js
+++ bin/clojs.js
@@ -1,16 +1,44 @@
+var fs = require.call(this, 'fs');
+var path = require.call(this, 'path');
+var util = require.call(this, 'util');
 var closer = require.call(this, '../lib/src/index');
 var escodegen = require.call(this, 'escodegen');
 var optionator = require.call(this, 'optionator');
 var core = typeof closer['core'] === 'function' && closer['core'].length === 0 ? closer['core']() : closer['core'];
 var closerAssertions = typeof closer['assertions'] === 'function' && closer['assertions'].length === 0 ? closer['assertions']() : closer['assertions'];
-var fs = require.call(this, 'fs');
-var path = require.call(this, 'path');
-var proc = core.js_$__$GT_clj.call(this, typeof process['argv'] === 'function' && process['argv'].length === 0 ? process['argv']() : process['argv']);
-var file = core.nth.call(this, proc, 2);
-var code = typeof fs['readFileSync'](file)['toString'] === 'function' && fs['readFileSync'](file)['toString'].length === 0 ? fs['readFileSync'](file)['toString']() : fs['readFileSync'](file)['toString'];
-var ast = closer['parse'](code, core.clj_$__$GT_js.call(this, core.hash_$_map(core.keyword('coreIdentifier'), 'core')));
-var compiled = escodegen['generate'](ast);
-fs['writeFile'](core.apply.call(this, core.str, path['dirname'](file), '/', path['basename'](file, '.cljs'), '.js'), compiled, function (err) {
+var opt = optionator.call(this, core.clj_$__$GT_js.call(this, core.hash_$_map(core.keyword('prepend'), 'Usage: cljsc [option]... [file]...\n\nUse \'cljsc\' with no options to start REPL.', core.keyword('append'), 'Version {{version}}\n<http://clojs.org/>', core.keyword('helpStyle'), core.hash_$_map(core.keyword('maxPadFactor'), 1.9), core.keyword('positionalAnywhere'), false, core.keyword('options'), core.vector(core.hash_$_map(core.keyword('option'), 'version', core.keyword('alias'), 'v', core.keyword('type'), 'Boolean', core.keyword('description'), 'display version'), core.hash_$_map(core.keyword('option'), 'help', core.keyword('alias'), 'h', core.keyword('type'), 'Boolean', core.keyword('description'), 'display this help message'), core.hash_$_map(core.keyword('option'), 'compile', core.keyword('alias'), 'c', core.keyword('type'), 'Boolean', core.keyword('description'), 'compile to JavaScript and save as .js files'), core.hash_$_map(core.keyword('option'), 'eval', core.keyword('alias'), 'e', core.keyword('type'), 'code::String', core.keyword('description'), 'pass as string from the command line as input')))));
+var die_$BANG_ = function (message) {
     closerAssertions.arity(1, arguments.length);
-    return core.not.call(this, core.nil_$QMARK_.call(this, err)) ? console['log'](err) : null;
-});
+    console['error'](message);
+    return process['exit'](1);
+};
+var compile_$_script_$_from_$_str = function (code) {
+    closerAssertions.arity(1, arguments.length);
+    return function () {
+        var ast = closer['parse'](code, core.clj_$__$GT_js.call(this, core.hash_$_map(core.keyword('coreIdentifier'), 'core')));
+        return escodegen['generate'](ast);
+    }.call(this);
+};
+var compile_$_script = function (file) {
+    closerAssertions.arity(1, arguments.length);
+    return function () {
+        var code = typeof fs['readFileSync'](file)['toString'] === 'function' && fs['readFileSync'](file)['toString'].length === 0 ? fs['readFileSync'](file)['toString']() : fs['readFileSync'](file)['toString'];
+        var compiled = compile_$_script_$_from_$_str.call(this, code);
+        return fs['writeFile'](core.apply.call(this, core.str, path['dirname'](file), '/', path['basename'](file, '.cljs'), '.js'), compiled, function (err) {
+            closerAssertions.arity(1, arguments.length);
+            return core.not.call(this, core.nil_$QMARK_.call(this, err)) ? console['log'](err) : null;
+        });
+    }.call(this);
+};
+var proc = core.js_$__$GT_clj.call(this, typeof process['argv'] === 'function' && process['argv'].length === 0 ? process['argv']() : process['argv']);
+if (core._$EQ_.call(this, core.count.call(this, proc), 2))
+    var closer = require.call(this, '../lib/src/start-repl');
+else
+    try {
+        (function () {
+            var command_$_options = (typeof opt['parse'] === 'function' && opt['parse'].length === 0 ? opt['parse']() : opt['parse']).call(this, typeof process['argv'] === 'function' && process['argv'].length === 0 ? process['argv']() : process['argv']);
+            return (typeof command_$_options['compile'] === 'function' && command_$_options['compile'].length === 0 ? command_$_options['compile']() : command_$_options['compile']) ? compile_$_script.call(this, core.first.call(this, typeof command_$_options['_'] === 'function' && command_$_options['_'].length === 0 ? command_$_options['_']() : command_$_options['_'])) : (typeof command_$_options['help'] === 'function' && command_$_options['help'].length === 0 ? command_$_options['help']() : command_$_options['help']) ? core.println.call(this, (typeof opt['generateHelp'] === 'function' && opt['generateHelp'].length === 0 ? opt['generateHelp']() : opt['generateHelp']).call(this, core.clj_$__$GT_js.call(this, core.hash_$_map(core.keyword('interpolate'), core.hash_$_map(core.keyword('version'), '0.0.0'))))) : (typeof command_$_options['version'] === 'function' && command_$_options['version'].length === 0 ? command_$_options['version']() : command_$_options['version']) ? core.println.call(this, 'version 0.0.0') : (typeof command_$_options['eval'] === 'function' && command_$_options['eval'].length === 0 ? command_$_options['eval']() : command_$_options['eval']) ? compile_$_script_$_from_$_str.call(this, typeof command_$_options['_'] === 'function' && command_$_options['_'].length === 0 ? command_$_options['_']() : command_$_options['_']) : null;
+        }.call(this));
+    } catch (e) {
+        die_$BANG_.call(this, typeof e['message'] === 'function' && e['message'].length === 0 ? e['message']() : e['message']);
+    }
